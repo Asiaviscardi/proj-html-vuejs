ddba5a9df56b3ba924826a3273239fb3173ac70f
Asiaviscardi <asia.viscardii@gmail.com>
2023-10-19 15:15:39 +0200

    termine struttura prima e seconda section main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,10 +7,85 @@ export default {
 
 <template>
 
-    <h1>Main</h1>
+    <div class="main-container">
+
+        <div class="first-section">
+
+            <div class="services-container">
+                
+                services
+
+            </div>
+
+        </div>
+
+        <div class="second-section">
+
+            <div class="img-container">
+
+                <div class="immagine">img</div>
+
+                <div class="immagine">img</div>
+
+                <div class="immagine">img</div>
+
+                <div class="immagine">img</div>
+
+            </div>
+
+            <div class="learn">
+
+                <div class="learn-components">sdf</div>
+
+            </div>
+
+        </div>
+
+    </div>
   
 </template>
 
-<style>
+<style scoped lang="scss">
+
+.first-section{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: azure;
+
+    .services-container{
+        width: 80%;
+        background-color: aqua;
+    }
+}
+
+.second-section{
+    width: 100%;
+    background-color: azure;
+
+    .img-container{
+        width: 100%;
+        display: flex;
+
+        .immagine{
+            width: 25%;
+            border: 1px solid black;
+
+        }
+    }
+
+    .learn{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        background-color: bisque;
+
+        .learn-components{
+            width: 80%;
+            background-color: cornflowerblue;
+        }
+    }
+
+}
 
 </style>
